48f377398871ac71c4c4cfe59db2efe37142575d
Szymon Jankowski <63104347+SJEnvi@users.noreply.github.com>
2022-05-22 11:25:22 +0200

    Add files via upload

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -1,13 +1,13 @@
 
-resource "azurerm_resource_group" "WSB-Szymon" {
+resource "azurerm_resource_group" "wsb_szymon" {
   name     = var.azurerm_resource_group_name
   location = var.azurerm_resource_group_location
 }
 
 resource "azurerm_storage_account" "azure_account" {
   name                     = var.account_name
-  resource_group_name      = azurerm_resource_group.WSB-Szymon.name
-  location                 = azurerm_resource_group.WSB-Szymon.location
+  resource_group_name      = azurerm_resource_group.wsb_szymon.name
+  location                 = azurerm_resource_group.wsb_szymon.location
   account_tier             = var.azurem_account_tier
   account_kind             = var.azurem_account_kind
   account_replication_type = var.azurem_account_replication
@@ -19,16 +19,16 @@ resource "azurerm_storage_account" "azure_account" {
 
 resource "azurerm_service_plan" "azure_plan" {
   name                = var.azurem_plan_name
-  resource_group_name = azurerm_resource_group.WSB-Szymon.name
-  location            = azurerm_resource_group.WSB-Szymon.location
+  resource_group_name = azurerm_resource_group.wsb_szymon.name
+  location            = azurerm_resource_group.wsb_szymon.location
   os_type             = var.azurem_plan_os
   sku_name            = var.azurem_plan_sku
 }
 
-resource "azurerm_linux_function_app" "function-app" {
+resource "azurerm_linux_function_app" "function_app" {
   name                = var.azurem_app_name
-  resource_group_name = azurerm_resource_group.WSB-Szymon.name
-  location            = azurerm_resource_group.WSB-Szymon.location
+  resource_group_name = azurerm_resource_group.wsb_szymon.name
+  location            = azurerm_resource_group.wsb_szymon.location
 
   storage_account_name = azurerm_storage_account.azure_account.name
   service_plan_id      = azurerm_service_plan.azure_plan.id
